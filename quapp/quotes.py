67c2cce10643871f34bb5b90e8550a1183fff5aa
filwie <filip.wiechec@gmail.com>
2019-08-02 02:35:46 +0200

    Do not use quotes too short or too long

--- quapp/quotes.py
+++ quapp/quotes.py
@@ -11,11 +11,11 @@ RE_SENTENCE = re.compile(r'(?:[A-Za-z]+[\ .\',\"!:]{0,3}){3,}.*')
 def get_random_quote():
     def find_quote(quotes_soup):
         for q in quotes_soup.find_all('li'):
-            if RE_SENTENCE.match(q.text):
+            if RE_SENTENCE.match(q.text) and 30 <= len(q.text) <= 300:
                 return q.text.split('\n', 1)[0]
         return 'Changing was necessary. Change was right.' \
                'He was all in favour of change.' \
-               'What he was dead against was things not staying the same.'
+               'What he was dead against was things not staying the same.', 'Terry Pratchett'
     quotes_html = requests.get(WIKIQUOTE_RANDOM_URL).text
     quotes_soup = BeautifulSoup(quotes_html, 'html.parser')
     author = quotes_soup.title.string.replace(' - Wikiquote', '')
